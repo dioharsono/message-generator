0e10687214df558d462ab140117e904d145431c7
dioharsono-admin <dioaldinoharsono@gmail.com>
2025-06-19 08:25:20 +0200

    add the javascript file containing the 3 arrays needed

---
+++ message-generator.js
@@ -0,0 +1,34 @@
+// Array of the possible messages
+
+const vibes = [
+  "calm as a monk",
+  "chaotic like a raccoon at 3AM",
+  "vibing like it’s 1999",
+  "in a dangerously productive mood",
+  "lost but aesthetically pleasing",
+  "main character energy",
+  "built different today"
+];
+
+const suggestions = [
+  "listen to lo-fi while pretending you’re in a movie",
+  "take a walk and romanticize your life",
+  "make a playlist called 'emotional damage'",
+  "code like a beast for 30 mins",
+  "re-watch that comfort show you always revisit",
+  "touch grass (seriously)",
+  "learn one useless but cool fact"
+];
+
+const fortunes = [
+  "A pigeon will guide you to success.",
+  "Beware of the color yellow today.",
+  "Your internet will betray you at a critical moment.",
+  "A bug in your code is a bug in your soul.",
+  "Luck is coming. It’s just stuck in traffic.",
+  "Someone you ignored on LinkedIn will become your boss.",
+  "You’ll win a debate with a toaster."
+];
+
+
+console.log("test");
